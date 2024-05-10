319db6c221b492329c5ab8ba7fe3923448cde0b9
Luca Di Bello <info@lucadibello.ch>
2024-05-10 23:49:04 +0200

    Skeleton solver class

---
+++ src/backend/modules/solver/movers.py
@@ -0,0 +1,6 @@
+# this class will contain the solver for the movers problem!
+
+
+def solve_problem(n_movers: int, n_floors: int, forniture: list[Forniture]):
+    # Start solver with the data
+    pass
