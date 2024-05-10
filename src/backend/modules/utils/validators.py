e3e6bfc3c1d472454cd995a8db7251186d979b61
Luca Di Bello <info@lucadibello.ch>
2024-05-11 00:24:19 +0200

    Added validator for forniture JSOn

---
+++ src/backend/modules/utils/validators.py
@@ -0,0 +1,16 @@
+from modules.models.api import Forniture
+
+
+def sanitize_string(s: str) -> str:
+    # Remove all non-alphanumeric characters from the string
+    return "".join(filter(str.isalnum, s))
+
+
+def validate_forniture(n_floors: int, forniture: list[Forniture]) -> bool:
+    # Ensure that the forniture list only containts valid floors
+    for f in forniture:
+        # Sanitize the name of the forniture
+        f.name = sanitize_string(f.name)
+        if not f.name or f.floor < 0 or f.floor >= n_floors:
+            return False
+    return True
