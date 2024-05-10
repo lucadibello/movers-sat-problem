4b556d5b40fb93ce877880496e649f494188ca9c
Luca Di Bello <info@lucadibello.ch>
2024-05-11 00:23:48 +0200

    Created API response builder

--- src/backend/modules/models/api.py
+++ src/backend/modules/models/api.py
@@ -1,6 +1,26 @@
 from pydantic import BaseModel
+from typing import Optional, TypeVar, TypedDict
 
 
 class Forniture(BaseModel):
     name: str
     floor: int
+
+
+T = TypeVar("T", dict, list)
+
+
+class Response[K](TypedDict):
+    success: bool
+    message: str
+    data: Optional[K]
+
+
+def build_response(
+    success: bool, message: str, data: Optional[T] = None
+) -> Response[T]:
+    response = {"success": success, "message": message}
+    # If data is provided, add it to the response
+    if data:
+        response["data"] = data
+    return Response[T](**response)
