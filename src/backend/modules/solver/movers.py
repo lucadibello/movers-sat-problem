0e1b47169d51a8acb71bdbdb61ef1d161876e6ff
Luca Di Bello <info@lucadibello.ch>
2024-05-11 00:23:59 +0200

    Added meaningful comment

--- src/backend/modules/solver/movers.py
+++ src/backend/modules/solver/movers.py
@@ -1,6 +1,7 @@
 # this class will contain the solver for the movers problem!
+from modules.models.api import Forniture
 
 
 def solve_problem(n_movers: int, n_floors: int, forniture: list[Forniture]):
-    # Start solver with the data
+    # Configure z3 solver and the the problem!
     pass
